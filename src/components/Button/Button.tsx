e293f94860a4774c2ec4703ee898f6352d504b47
Lucas Lopes <lucass.code@gmail.com>
2022-12-11 10:19:43 -0500

    refactor: button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -17,50 +17,11 @@ const Button: React.FC<ButtonProps> = ({
   className = '',
   ...props
 }) => {
-  const style = [
-    'focus:ring-4',
-    'focus:ring-gray-200',
-    'dark:focus:ring-gray-700',
-  ];
-  if (variant === 'contained') {
-    if (!className.includes('bg')) {
-      style.push(
-        ...[
-          'bg-gray-50',
-          `hover:bg-gray-100`,
-          `dark:bg-gray-600`,
-          `dark:hover:bg-gray-700`,
-          'text-black',
-          'dark:text-white',
-        ]
-      );
-    }
-  } else if (variant === 'outlined') {
-    style.push(
-      ...[
-        'border',
-        'border-gray-200',
-        `hover:border-gray-300`,
-        `dark:border-gray-600`,
-        `dark:hover:border-gray-700`,
-        'text-black',
-      ]
-    );
-  } else if (variant === 'text') {
-    style.push(...[]);
-  }
-
-  if (size === 'medium') {
-    style.push(...['py-2.5', 'px-5', 'text-sm', 'font-medium']);
-  } else if (size === 'small') {
-    style.push(...['py-2', 'px-3', 'text-sm', 'font-medium']);
-  } else if (size === 'large') {
-    style.push(...['py-3', 'px-5', 'text-base', 'font-medium']);
-  }
-
-  className = className.concat(` ${style.join(' ')}`);
   return (
-    <button {...props} className={className}>
+    <button
+      {...props}
+      className={`button button-variant-${variant} button-size-${size} ${className}`}
+    >
       {children}
     </button>
   );
